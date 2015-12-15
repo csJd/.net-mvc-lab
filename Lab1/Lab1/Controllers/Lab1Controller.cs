177e73113b9f095a92a9ae07c6ce58c1b4f7d59f
dd <ddwust@gmail.com>
2015-12-15 14:19:51 +0800

    add lab1 controller

---
+++ Lab1/Lab1/Controllers/Lab1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class Lab1Controller : Controller
+    {
+        // GET: Lab1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
