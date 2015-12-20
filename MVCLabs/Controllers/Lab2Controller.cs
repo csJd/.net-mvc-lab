28a3edbc78273ffcbf8980c86067ed1e2e826d46
dd <ddwust@gmail.com>
2015-12-20 16:16:30 +0800

    add the lab2 controller

---
+++ MVCLabs/Controllers/Lab2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLabs.Controllers
+{
+    public class Lab2Controller : Controller
+    {
+        // GET: Lab2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
